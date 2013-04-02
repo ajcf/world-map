35364a4e7b34019d596e2fe7d7d699b777f5082a
Amanda Cadwell-Frost <acadwellfrost@gmail.com>
2013-04-02 08:28:27 -0400

    switched to rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorldMap::Application.config.secret_token = 'cb685eb26588502f104d8de6f3a909dcc3d73fbf007eb1fd9f5218376c0a7c2651b72c31a3e6aa63c99e89c8c1c5ed6947ee58cc4ef59c0dd3b883c3ed18b339'
